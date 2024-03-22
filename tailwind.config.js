a6842fe00c91096f7aa0c32aba0fd8457b6ef653
Imamuzzaman Tasin <imamujjamantasin@gmail.com>
2024-03-23 01:25:01 +0600

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        gilroy:["gilroy-bold , sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
+
